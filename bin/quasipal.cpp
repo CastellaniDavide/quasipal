10f33cf90d6e89b5e04b8442faf9928c7d2b32d2
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-05-16 13:21:15 +0200

    Created bin/quasipal.cpp

---
+++ bin/quasipal.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file quasipal.cpp
+ *
+ * @version 01.01 2021516
+ *
+ * @brief https://training.olinfo.it/#/task/quasipal/statement
+ *
+ * @ingroup quasipal
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+
+// Includes
+#include <bits/stdc++.h>
+using namespace std;
+
+// Variabiles
+int N;
+
+// Main code
+int main()
+{
+  // Cncomment the following lines if you want to read/write from files
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt", "w", stdout);
+
+  // Input
+  cin >> N;
+
+  // Code
+  // ...
+
+  // Output
+  cout << N << endl;
+
+  // End
+  return 0;
+}
