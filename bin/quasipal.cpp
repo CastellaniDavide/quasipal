2dc90ae71fc9abe43860ee4ac5118c43ff5cdc85
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-05-16 15:14:14 +0200

    Auto-update

--- bin/quasipal.cpp
+++ bin/quasipal.cpp
@@ -1,42 +1,78 @@
-/**
- * @file quasipal.cpp
- *
- * @version 01.01 2021516
- *
- * @brief https://training.olinfo.it/#/task/quasipal/statement
- *
- * @ingroup quasipal
- * (Note: this needs exactly one @defgroup somewhere)
- *
- * @author Castellani Davide
- *
- * Contact: contacts@castellanidavide.it
- *
- */
-
-// Includes
 #include <bits/stdc++.h>
 using namespace std;
+#define DEBUG
 
-// Variabiles
-int N;
+int N, M;
+vector<string> numbers;
+vector< vector<int> > poss;
 
-// Main code
+bool IsPalindrome(vector <int> combinations)
+{
+    for (size_t i = 0; i < M / 2; ++i)
+    {
+        for (size_t j = 0; j < N; ++j)
+        {
+            if (numbers[combinations[i]][j] != numbers[combinations[(M - 1) - i]][j]
+                    && numbers[combinations[i]][j] != '0'
+                    && numbers[combinations[(M - 1) - i]][j] != '0')
+                return false;
+        }
+    }
+    return true;
+}
+
+void add_possibility (vector<bool> used, vector <int> last, int u)
+{
+    if (u == used.size())
+    {
+        poss.push_back(last);
+    }
+    else
+    {
+        for (size_t i = 0; i < used.size(); ++i)
+        {
+            if (!used[i])
+            {
+                used[i] = true;
+                vector<int> tmp(last);
+                tmp.push_back(i);
+
+                add_possibility(used, tmp, u + 1);
+
+                used[i] = false;
+            }
+        }
+    }
+}
+
 int main()
 {
-  // Cncomment the following lines if you want to read/write from files
-  // freopen("input.txt", "r", stdin);
-  // freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+#ifndef DEBUG
+    freopen("output.txt", "w", stdout);
+#endif // DEBUG
 
-  // Input
-  cin >> N;
+    cin >> M >> N;
+    numbers.resize(M);
 
-  // Code
-  // ...
+    for (size_t i = 0; i < M; ++i)
+    {
+        cin >> numbers[i];
+    }
 
-  // Output
-  cout << N << endl;
+    vector <bool> tmp (M, false);
+    vector <int> tmp2;
+    add_possibility(tmp, tmp2, 0);
 
-  // End
-  return 0;
+    for (size_t i = 0; i < poss.size(); ++i)
+    {
+        if(IsPalindrome(poss[i]) == true)
+        {
+            for (size_t j = 0; j < M; ++j)
+            {
+                cout << numbers[poss[i][j]] << endl;
+            }
+            return 0;
+        }
+    }
 }
